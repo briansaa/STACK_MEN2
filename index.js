87a10ed1f82c14634abef7295a4fd5d2ec1c6f45
Mac16 <alumno@MacBook-Pro-16.local>
2024-03-21 11:10:20 -0500

    stack_men2

---
+++ index.js
@@ -0,0 +1,10 @@
+const express=require('express')
+const app  =express()
+const routes=require('./rutas.js')
+const port = 3000
+
+
+app.use(routes)//app.use('/usuario,routes')
+app.listen(port,()=>{
+    console.log('server on port ${3000}')
+})
